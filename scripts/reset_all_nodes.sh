57a5a9e3b458ade1dd2d78237ed170f35bfbeb5c
Richard Myers <remyers@yakshaver.org>
2022-02-02 14:59:18 +0100

    kill eclair nodes before resetting

--- scripts/reset_all_nodes.sh
+++ scripts/reset_all_nodes.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# kill eclair nodes
+pkill -f alice
+pkill -f bob
+pkill -f carol
+pkill -f dave
+
+# remove files
 (cd .eclair && ./reset_nodes.sh)
 (cd .lightning && ./reset_nodes.sh)
 (cd .lnd && ./reset_nodes.sh)
